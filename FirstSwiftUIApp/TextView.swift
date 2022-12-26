0a495ad6689f57f8080a34f9d40a1d32a4acd58c
Yun <duwls0349@naver.com>
2022-12-27 00:13:52 +0900

    [Feat] modifier

--- FirstSwiftUIApp/TextView.swift
+++ FirstSwiftUIApp/TextView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct TextView: View {
     var body: some View {
         Text("SwiftUI")
+            .font(.system(size: 40, weight: .bold, design: .default))
     }
 }
 
@@ -18,16 +19,24 @@ struct ButtonView: View {
         Button {
             print("눌렸습니다.")
         } label: {
-            Text("버튼이 눌렸습니다.")
+            Text("눌러주세요.")
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .foregroundColor(.white)
         }
-
+        .padding()
+        .frame(height: 100)
+        .background(.yellow)
+        .cornerRadius(20)
     }
 }
-
+//modifier 순서가 영향을 줄 수도 있다.
 struct ImageView: View {
     var body: some View {
         Image(systemName: "sun.max.fill")
-    }
+            .renderingMode(.original) //MultiColor가 yellow
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+    }//Image -> Image -> View로 반환
 }
 
 struct TextView_Previews: PreviewProvider {
